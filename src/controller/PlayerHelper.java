d41038399750b2f6ecb2bfb13b8d22eb36f79a7a
JessAskeland <36720058+JessAskeland@users.noreply.github.com>
2018-02-24 22:43:12 -0600

    Step 2; add player

--- src/controller/PlayerHelper.java
+++ src/controller/PlayerHelper.java
@@ -1,8 +1,20 @@
 package controller;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import model.Team;
+
 public class PlayerHelper {
 	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("BowlingLeagueAskeland");
+	
+	public void insertItem(Team toAdd) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toAdd);
+		em.getTransaction().commit();
+		em.close();
+	}
 }

f47d041ea5d811d3d138d9cab9a0ce55b0faa6a1
swesthoff <36612586+swesthoff@users.noreply.github.com>
2018-02-26 12:48:16 -0600

    Added Player informaton

--- src/controller/PlayerHelper.java
+++ src/controller/PlayerHelper.java
@@ -1,8 +1,43 @@
 package controller;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+
+import model.Player;
+
+
 
 public class PlayerHelper {
 	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("BowlingLeagueAskeland");
+	
+	
+	public void insertPlayer(Player toAdd) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toAdd);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public List<Player> showAllPlayers() {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		// creates the query but does not execute it.
+		TypedQuery<Player> allResults = em.createQuery("Select list_item from Player list_item", Player.class);
+		// Selects the info
+		List<Player> allPlayers = allResults.getResultList();
+		// need to close the entity manager
+		em.close();
+
+		return allPlayers;
+	}
+
 }
+
+
